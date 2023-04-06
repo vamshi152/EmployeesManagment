31acae6c20e85d5efbd97b5f55f49b1e0fb086d2
vamshi152 <vamshichowdary152@gmail.com>
2023-04-06 10:13:52 +0530

    code fix

--- src/main/java/com/employee/mangement/EmployeeDetailsApplication.java
+++ src/main/java/com/employee/mangement/EmployeeDetailsApplication.java
@@ -6,6 +6,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import static springfox.documentation.builders.PathSelectors.regex;
+
 
 @SpringBootApplication
 @ComponentScan(basePackages = { "com.employee"})
@@ -15,14 +21,13 @@ public class EmployeeDetailsApplication {
 		SpringApplication.run(EmployeeDetailsApplication.class, args);
 	}
 	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("*");
-			}
-		};
+	public Docket productApi() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("com.employee.management"))
+				.paths(regex("/rest.*"))
+           .build();
 	}
+
+
 
 }
--- src/main/java/com/employee/mangement/config/SwaggerConfig.java
+++ src/main/java/com/employee/mangement/config/SwaggerConfig.java
@@ -9,12 +9,16 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import static springfox.documentation.builders.PathSelectors.regex;
+
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
     @Bean
     public Docket productApi() {
         return new Docket(DocumentationType.SWAGGER_2).select()
-                .apis(RequestHandlerSelectors.basePackage("com.employee.management")).build();
-}
+                .apis(RequestHandlerSelectors.basePackage("com.employee.management"))
+                .paths(regex("/api/employees.*"))
+             .build();
+    }
 }
